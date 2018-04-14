ddc491ca3eb0334752cf06064756859b1d58eeae
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-04-14 14:52:50 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,9 +5,36 @@ class FiguresController < ApplicationController
     erb :'figures/index'
   end
 
-  get 'figures/new' do #CREATE: make new figures form
+  get '/figures/new' do #CREATE: make new figures form
     @landmark = Landmark.all
     @titles = Title.all
     erb :'figures/new'
   end
+
+  post '/figures' do #CREATE: creates the new instance of figures
+    @figure = Figure.new(params[:figure])
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+
+  end
+
+  get '/figures/:id' do #READ: view specific figure
+    @figure = Figure.find_by_id(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do #UPDATE: edit a specific figure
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/edit'
+  end
+
 end
